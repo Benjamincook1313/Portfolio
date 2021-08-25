42cecf1512bb3ccee7d272370cdfd7b34e1f549b
Benjamin <benjamincook1313@gmail.com>
2021-08-24 20:17:36 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
-// import AboutMe from './Components/AboutMe/AboutMe'
-// import Skills from './Components/Skills/Skills'
-// import Projects from './Components/Projects/Projects'
 import Nav from './Components/Nav/Nav'
 
 function App() {
--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -13,6 +13,27 @@ class AboutMe extends Component {
       <div className='AboutMe' id='about'>
         <h1 className='title'>About Me</h1>
         <div className='A-info'>
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="..." class="d-block w-100" alt="..."/>
+                </div>
+                <div class="carousel-item">
+                  <img src="..." class="d-block w-100" alt="..."/>
+                </div>
+                <div class="carousel-item">
+                  <img src="..." class="d-block w-100" alt="..."/>
+                </div>
+              </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
           <img className='A-image' src='https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/50739460_10218404922938372_3203143294611619840_n.jpg?_nc_cat=108&_nc_ht=scontent-lax3-2.xx&oh=35e54f7310aabc47a92be9940d0d6339&oe=5D76D46D' alt=''/>
           <div className='text-container'>
             <p className='A-text'>
