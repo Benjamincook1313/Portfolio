2eec93e25335cd6cad6d707ec6f17efe6453ac79
Benjamin <benjamincook1313@gmail.com>
2021-09-12 14:54:25 -0600

    Updates

---
+++ precache-manifest.2566bc40bbaa1a8c2c89aa3e48ed4032.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e9397959d56ad2c3cca18e71c3aa285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b303e7b425193325ec8",
+    "url": "/static/css/main.3b633bf6.chunk.css"
+  },
+  {
+    "revision": "3cdc3369068102313a7c",
+    "url": "/static/js/2.a65a5a1c.chunk.js"
+  },
+  {
+    "revision": "7b303e7b425193325ec8",
+    "url": "/static/js/main.d2c72a57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "777deb4ae20206fa31170dde74db65e6",
+    "url": "/static/media/64827663_2809731762387672_1789481639793917952_n copy.777deb4a.jpg"
+  },
+  {
+    "revision": "2648c0abb6e5d82bcb732b471cadd9d4",
+    "url": "/static/media/80093560_10221063967652828_1252318498219098112_o.2648c0ab.jpg"
+  },
+  {
+    "revision": "bdfcb6c0d3ca8f7ba3ba2be3dcb08623",
+    "url": "/static/media/Screen Shot 2019-12-06 at 5.17.57 PM.bdfcb6c0.png"
+  },
+  {
+    "revision": "25291133b93dd06af15332c6afe6e97a",
+    "url": "/static/media/Screen Shot 2019-12-06 at 5.19.21 PM.25291133.png"
+  },
+  {
+    "revision": "a3643209b0d88a936b99f79a6f3503c7",
+    "url": "/static/media/Screen Shot 2019-12-06 at 5.25.44 PM.a3643209.png"
+  },
+  {
+    "revision": "c19ef3e55a9620c780d646cb31f1f9d6",
+    "url": "/static/media/steelhead.c19ef3e5.jpg"
+  }
+]);
